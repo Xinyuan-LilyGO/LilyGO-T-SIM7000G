9b7e9f73be93dd9744da32eabdbe633e8fdc73ec
LilyGO <31160602+LilyGO@users.noreply.github.com>
2022-04-23 10:57:19 +0800

    perfect：Band lock and some simple AT instructions

--- examples/Arduino_TinyGSM/AllFunctions/AllFunctions.ino
+++ examples/Arduino_TinyGSM/AllFunctions/AllFunctions.ino
@@ -9,7 +9,7 @@
 #define SerialAT Serial1
 
 // See all AT commands, if wanted
- #define DUMP_AT_COMMANDS
+#define DUMP_AT_COMMANDS
 
 /*
    Tests enabled
@@ -59,268 +59,286 @@ int counter, lastIndex, numberOfPieces = 24;
 String pieces[24], input;
 
 
-void setup() {
-  // Set console baud rate
-  Serial.begin(115200);
-  delay(10);
+void setup()
+{
+    // Set console baud rate
+    Serial.begin(115200);
+    delay(10);
 
-  // Set LED OFF
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
+    // Set LED OFF
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
 
-  pinMode(PWR_PIN, OUTPUT);
-  digitalWrite(PWR_PIN, HIGH);
-  delay(300);
-  digitalWrite(PWR_PIN, LOW);
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, HIGH);
+    delay(300);
+    digitalWrite(PWR_PIN, LOW);
 
-  SPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
-  if (!SD.begin(SD_CS)) {
-    Serial.println("SDCard MOUNT FAIL");
-  } else {
-    uint32_t cardSize = SD.cardSize() / (1024 * 1024);
-    String str = "SDCard Size: " + String(cardSize) + "MB";
-    Serial.println(str);
-  }
-
-  Serial.println("\nWait...");
-
-  delay(1000);
-
-  SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
-
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  Serial.println("Initializing modem...");
-  if (!modem.restart()) {
-    Serial.println("Failed to restart modem, attempting to continue without restarting");
-  }
-
-
-     modem.sendAT("+CFUN=0 ");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG(" +CFUN=0  false ");
+    SPI.begin(SD_SCLK, SD_MISO, SD_MOSI, SD_CS);
+    if (!SD.begin(SD_CS)) {
+        Serial.println("SDCard MOUNT FAIL");
+    } else {
+        uint32_t cardSize = SD.cardSize() / (1024 * 1024);
+        String str = "SDCard Size: " + String(cardSize) + "MB";
+        Serial.println(str);
     }
 
-           modem.sendAT("+CFUN=1 ");
-    if (modem.waitResponse(10000L) != 1) {
-        DBG(" +CFUN=1  false ");
+    Serial.println("\nWait...");
+
+    delay(1000);
+
+    SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+
+    // Restart takes quite some time
+    // To skip it, call init() instead of restart()
+    Serial.println("Initializing modem...");
+    if (!modem.restart()) {
+        Serial.println("Failed to restart modem, attempting to continue without restarting");
     }
+
+
+
 
 
 }
 
-void loop() {
-  
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  Serial.println("Initializing modem...");
-  if (!modem.init()) {
-    Serial.println("Failed to restart modem, attempting to continue without restarting");
-  }
+void loop()
+{
 
-  String name = modem.getModemName();
-  delay(500);
-  Serial.println("Modem Name: " + name);
+    // Restart takes quite some time
+    // To skip it, call init() instead of restart()
+    Serial.println("Initializing modem...");
+    if (!modem.init()) {
+        Serial.println("Failed to restart modem, attempting to continue without restarting");
+    }
 
-  String modemInfo = modem.getModemInfo();
-  delay(500);
-  Serial.println("Modem Info: " + modemInfo);
+    String name = modem.getModemName();
+    delay(500);
+    Serial.println("Modem Name: " + name);
 
-  // Set SIM7000G GPIO4 LOW ,turn off GPS power
-  // CMD:AT+SGPIO=0,4,1,0
-  // Only in version 20200415 is there a function to control GPS power
-  modem.sendAT("+SGPIO=0,4,1,0");
+    String modemInfo = modem.getModemInfo();
+    delay(500);
+    Serial.println("Modem Info: " + modemInfo);
+
+    // Set SIM7000G GPIO4 LOW ,turn off GPS power
+    // CMD:AT+SGPIO=0,4,1,0
+    // Only in version 20200415 is there a function to control GPS power
+    modem.sendAT("+SGPIO=0,4,1,0");
     if (modem.waitResponse(10000L) != 1) {
         DBG(" SGPIO=0,4,1,0 false ");
     }
 
 #if TINY_GSM_TEST_GPRS
-  // Unlock your SIM card with a PIN if needed
-  if ( GSM_PIN && modem.getSimStatus() != 3 ) {
-    modem.simUnlock(GSM_PIN);
-  }
+    // Unlock your SIM card with a PIN if needed
+    if ( GSM_PIN && modem.getSimStatus() != 3 ) {
+        modem.simUnlock(GSM_PIN);
+    }
 #endif
 
-  /*
-    2 Automatic
-    13 GSM only
-    38 LTE only
-    51 GSM and LTE only
-  * * * */
-  String res;
-   res = modem.setNetworkMode(38);
-   if (res != "1") {
-      DBG("setNetworkMode  false ");
-            return ;
-        }
+    modem.sendAT("+CFUN=0 ");
+    if (modem.waitResponse(10000L) != 1) {
+        DBG(" +CFUN=0  false ");
+    }
+    delay(200);
 
-  /*
-    1 CAT-M
-    2 NB-Iot
-    3 CAT-M and NB-IoT
-  * * */
-   res = modem.setPreferredMode(2);
-   if (res != "1") {
+    /*
+      2 Automatic
+      13 GSM only
+      38 LTE only
+      51 GSM and LTE only
+    * * * */
+    String res;
+    res = modem.setNetworkMode(38);
+    if (res != "1") {
+        DBG("setNetworkMode  false ");
+        return ;
+    }
+    delay(200);
 
-     DBG("setPreferredMode  false ");
-            return ;
-        }
+    /*
+      1 CAT-M
+      2 NB-Iot
+      3 CAT-M and NB-IoT
+    * * */
+    res = modem.setPreferredMode(2);
+    if (res != "1") {
+
+        DBG("setPreferredMode  false ");
+        return ;
+    }
+    delay(200);
+
+    /*AT+CBANDCFG=<mode>,<band>[,<band>…]
+     * <mode> "CAT-M"   "NB-IOT"
+     * <band>  The value of <band> must is in the band list of getting from  AT+CBANDCFG=?
+     * For example, my SIM card carrier "NB-iot" supports B8.  I will configure +CBANDCFG= "Nb-iot ",8
+     */
+    /* modem.sendAT("+CBANDCFG=\"NB-IOT\",8 ");
+     if (modem.waitResponse(10000L) != 1) {
+         DBG(" +CBANDCFG=\"NB-IOT\" ");
+     }
+     delay(200);*/
+
+    modem.sendAT("+CFUN=1 ");
+    if (modem.waitResponse(10000L) != 1) {
+        DBG(" +CFUN=1  false ");
+    }
+    delay(200);
 
 #if TINY_GSM_TEST_GPRS
 
-  SerialAT.println("AT+CGDCONT?");
-  delay(500);
-  if (SerialAT.available()) {
-    input = SerialAT.readString();
-    for (int i = 0; i < input.length(); i++) {
-      if (input.substring(i, i + 1) == "\n") {
-        pieces[counter] = input.substring(lastIndex, i);
-        lastIndex = i + 1;
-        counter++;
-      }
-      if (i == input.length() - 1) {
-        pieces[counter] = input.substring(lastIndex, i);
-      }
-    }
-    // Reset for reuse
-    input = "";
-    counter = 0;
-    lastIndex = 0;
-
-    for ( int y = 0; y < numberOfPieces; y++) {
-      for ( int x = 0; x < pieces[y].length(); x++) {
-        char c = pieces[y][x];  //gets one byte from buffer
-        if (c == ',') {
-          if (input.indexOf(": ") >= 0) {
-            String data = input.substring((input.indexOf(": ") + 1));
-            if ( data.toInt() > 0 && data.toInt() < 25) {
-              modem.sendAT("+CGDCONT=" + String(data.toInt()) + ",\"IP\",\"" + String(apn) + "\",\"0.0.0.0\",0,0,0,0");
+    SerialAT.println("AT+CGDCONT?");
+    delay(500);
+    if (SerialAT.available()) {
+        input = SerialAT.readString();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.substring(i, i + 1) == "\n") {
+                pieces[counter] = input.substring(lastIndex, i);
+                lastIndex = i + 1;
+                counter++;
             }
-            input = "";
-            break;
-          }
-          // Reset for reuse
-          input = "";
+            if (i == input.length() - 1) {
+                pieces[counter] = input.substring(lastIndex, i);
+            }
         }
-        else {
-          input += c;
+        // Reset for reuse
+        input = "";
+        counter = 0;
+        lastIndex = 0;
+
+        for ( int y = 0; y < numberOfPieces; y++) {
+            for ( int x = 0; x < pieces[y].length(); x++) {
+                char c = pieces[y][x];  //gets one byte from buffer
+                if (c == ',') {
+                    if (input.indexOf(": ") >= 0) {
+                        String data = input.substring((input.indexOf(": ") + 1));
+                        if ( data.toInt() > 0 && data.toInt() < 25) {
+                            modem.sendAT("+CGDCONT=" + String(data.toInt()) + ",\"IP\",\"" + String(apn) + "\",\"0.0.0.0\",0,0,0,0");
+                        }
+                        input = "";
+                        break;
+                    }
+                    // Reset for reuse
+                    input = "";
+                } else {
+                    input += c;
+                }
+            }
         }
-      }
+    } else {
+        Serial.println("Failed to get PDP!");
     }
-  } else {
-    Serial.println("Failed to get PDP!");
-  }
 
 
-  Serial.println("\n\n\nWaiting for network...");
-  if (!modem.waitForNetwork()) {
-    delay(10000);
-    return;
-  }
+    Serial.println("\n\n\nWaiting for network...");
+    if (!modem.waitForNetwork()) {
+        delay(10000);
+        return;
+    }
 
-  if (modem.isNetworkConnected()) {
-    Serial.println("Network connected");
-  }
+    if (modem.isNetworkConnected()) {
+        Serial.println("Network connected");
+    }
 
-  Serial.println("\n---Starting GPRS TEST---\n");
-  Serial.println("Connecting to: " + String(apn));
-  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-    delay(10000);
-    return;
-  }
+    Serial.println("\n---Starting GPRS TEST---\n");
+    Serial.println("Connecting to: " + String(apn));
+    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+        delay(10000);
+        return;
+    }
 
-  Serial.print("GPRS status: ");
-  if (modem.isGprsConnected()) {
-    Serial.println("connected");
-  } else {
-    Serial.println("not connected");
-  }
+    Serial.print("GPRS status: ");
+    if (modem.isGprsConnected()) {
+        Serial.println("connected");
+    } else {
+        Serial.println("not connected");
+    }
 
-  String ccid = modem.getSimCCID();
-  Serial.println("CCID: " + ccid);
+    String ccid = modem.getSimCCID();
+    Serial.println("CCID: " + ccid);
 
-  String imei = modem.getIMEI();
-  Serial.println("IMEI: " + imei);
+    String imei = modem.getIMEI();
+    Serial.println("IMEI: " + imei);
 
-  String cop = modem.getOperator();
-  Serial.println("Operator: " + cop);
+    String cop = modem.getOperator();
+    Serial.println("Operator: " + cop);
 
-  IPAddress local = modem.localIP();
-  Serial.println("Local IP: " + String(local));
+    IPAddress local = modem.localIP();
+    Serial.println("Local IP: " + String(local));
 
-  int csq = modem.getSignalQuality();
-  Serial.println("Signal quality: " + String(csq));
+    int csq = modem.getSignalQuality();
+    Serial.println("Signal quality: " + String(csq));
 
-  SerialAT.println("AT+CPSI?");     //Get connection type and band
-  delay(500);
-  if (SerialAT.available()) {
-    String r = SerialAT.readString();
-    Serial.println(r);
-  }
+    SerialAT.println("AT+CPSI?");     //Get connection type and band
+    delay(500);
+    if (SerialAT.available()) {
+        String r = SerialAT.readString();
+        Serial.println(r);
+    }
 
-  Serial.println("\n---End of GPRS TEST---\n");
+    Serial.println("\n---End of GPRS TEST---\n");
 #endif
 
 #if TINY_GSM_TEST_GPRS
-  modem.gprsDisconnect();
-  if (!modem.isGprsConnected()) {
-    Serial.println("GPRS disconnected");
-  } else {
-    Serial.println("GPRS disconnect: Failed.");
-  }
+    modem.gprsDisconnect();
+    if (!modem.isGprsConnected()) {
+        Serial.println("GPRS disconnected");
+    } else {
+        Serial.println("GPRS disconnect: Failed.");
+    }
 #endif
 
 #if TINY_GSM_TEST_GPS
-  Serial.println("\n---Starting GPS TEST---\n");
-  // Set SIM7000G GPIO4 HIGH ,turn on GPS power
-  // CMD:AT+SGPIO=0,4,1,1
-  // Only in version 20200415 is there a function to control GPS power
-  modem.sendAT("+SGPIO=0,4,1,1");
+    Serial.println("\n---Starting GPS TEST---\n");
+    // Set SIM7000G GPIO4 HIGH ,turn on GPS power
+    // CMD:AT+SGPIO=0,4,1,1
+    // Only in version 20200415 is there a function to control GPS power
+    modem.sendAT("+SGPIO=0,4,1,1");
     if (modem.waitResponse(10000L) != 1) {
         DBG(" SGPIO=0,4,1,1 false ");
     }
-  modem.enableGPS();
-  float lat,  lon;
-  while (1) {
-    if (modem.getGPS(&lat, &lon)) {
-      Serial.printf("lat:%f lon:%f\n", lat, lon);
-      break;
-    } else {
-      Serial.print("getGPS ");
-      Serial.println(millis());
+    modem.enableGPS();
+    float lat,  lon;
+    while (1) {
+        if (modem.getGPS(&lat, &lon)) {
+            Serial.printf("lat:%f lon:%f\n", lat, lon);
+            break;
+        } else {
+            Serial.print("getGPS ");
+            Serial.println(millis());
+        }
+        delay(2000);
     }
-    delay(2000);
-  }
-  modem.disableGPS();
+    modem.disableGPS();
 
-  // Set SIM7000G GPIO4 LOW ,turn off GPS power
-  // CMD:AT+SGPIO=0,4,1,0
-  // Only in version 20200415 is there a function to control GPS power
-  modem.sendAT("+SGPIO=0,4,1,0");
-  if (modem.waitResponse(10000L) != 1) {
+    // Set SIM7000G GPIO4 LOW ,turn off GPS power
+    // CMD:AT+SGPIO=0,4,1,0
+    // Only in version 20200415 is there a function to control GPS power
+    modem.sendAT("+SGPIO=0,4,1,0");
+    if (modem.waitResponse(10000L) != 1) {
         DBG(" SGPIO=0,4,1,0 false ");
     }
-  Serial.println("\n---End of GPRS TEST---\n");
+    Serial.println("\n---End of GPRS TEST---\n");
 #endif
 
 
 #if TINY_GSM_POWERDOWN
-  // Try to power-off (modem may decide to restart automatically)
-  // To turn off modem completely, please use Reset/Enable pins
+    // Try to power-off (modem may decide to restart automatically)
+    // To turn off modem completely, please use Reset/Enable pins
     modem.sendAT("+CPOWD=1");
-  if (modem.waitResponse(10000L) != 1) {
+    if (modem.waitResponse(10000L) != 1) {
         DBG("+CPOWD=1");
     }
-  modem.poweroff();
-  Serial.println("Poweroff.");
+    modem.poweroff();
+    Serial.println("Poweroff.");
 #endif
 
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  delay(200);
-  esp_deep_sleep_start();
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    delay(200);
+    esp_deep_sleep_start();
 
-  // Do nothing forevermore
-  while (true) {
-    modem.maintain();
-  }
+    // Do nothing forevermore
+    while (true) {
+        modem.maintain();
+    }
 }
