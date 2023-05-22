be1b3015ca89888afd5731108275c8e791b4259a
lewishe <lewishe@outlook.com>
2023-05-22 17:04:04 +0800

    Update example

--- examples/Arduino_TinyGSM/ResetModem/ResetModem.ino
+++ examples/Arduino_TinyGSM/ResetModem/ResetModem.ino
@@ -28,14 +28,39 @@ TinyGsm modem(debugger);
 TinyGsm modem(SerialAT);
 #endif
 
+
 bool reply = false;
+
+uint32_t TinyGsmAutoBaud()
+{
+    static uint32_t rates[] = {115200, 57600,  38400, 19200, 9600,  74400, 74880,
+                               230400, 460800, 2400,  4800,  14400, 28800
+                              };
+
+    for (uint8_t i = 0; i < sizeof(rates) / sizeof(rates[0]); i++) {
+        uint32_t rate = rates[i];
+        if (rate < 9600 || rate > 115200) continue;
+
+        DBG("Trying baud rate", rate, "...");
+        SerialAT.updateBaudRate(rate);
+        delay(10);
+        for (int j = 0; j < 10; j++) {
+            SerialAT.print("AT\r\n");
+            String input = SerialAT.readString();
+            if (input.indexOf("OK") >= 0) {
+                DBG("Modem responded at rate", rate);
+                return rate;
+            }
+        }
+    }
+    return 0;
+}
 
 void setup()
 {
     // Set console and modem baud rate
     Serial.begin(9600);
-    SerialAT.begin(9600, SERIAL_8N1, PIN_RX, PIN_TX);
-    delay(500);
+
 
     // Set-up modem  power pin
     Serial.println("\nStarting Up Modem...");
@@ -45,6 +70,16 @@ void setup()
     delay(1000);
     digitalWrite(PWR_PIN, LOW);
     delay(10000);                 //Wait for the SIM7000 communication to be normal, and will quit when receiving OK
+
+
+    SerialAT.begin(9600, SERIAL_8N1, PIN_RX, PIN_TX);
+
+
+    if (TinyGsmAutoBaud() == 0) {
+        Serial.println("Failed start modem ! Hardware is not respone!");
+        while (1)delay(500);
+    }
+
 
     int i = 10;
     Serial.println("\nTesting Modem Response...\n");
@@ -66,9 +101,7 @@ void setup()
     Serial.println("****\n");
 
     if (reply) {
-
         bool ret = modem.factoryDefault();
-
         Serial.println(F("\n***********************************************************"));
         Serial.print  (F(" Reset settings to Factory Default: "));
         Serial.println((ret) ? "OK" : "FAIL");
@@ -82,5 +115,11 @@ void setup()
 
 void loop()
 {
-
+    while (SerialAT.available()) {
+        Serial.write(SerialAT.read());
+    }
+    while (Serial.available()) {
+        SerialAT.write(Serial.read());
+    }
+    delay(1);
 }
