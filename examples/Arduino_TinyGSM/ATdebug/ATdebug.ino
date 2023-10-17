6266333a9ebe833656e30c22f727a7adb0e8f090
lewishe <lewishe@outlook.com>
2023-10-17 17:25:42 +0800

    Update ATDebug

--- examples/Arduino_TinyGSM/ATdebug/ATdebug.ino
+++ examples/Arduino_TinyGSM/ATdebug/ATdebug.ino
@@ -16,6 +16,33 @@
 
 bool reply = false;
 
+uint32_t dectModemBaud()
+{
+    static uint32_t rates[] = {115200, 57600,  38400, 19200, 9600,  74400, 74880,
+                               230400, 460800, 2400,  4800,  14400, 28800
+                              };
+
+    for (uint8_t i = 0; i < sizeof(rates) / sizeof(rates[0]); i++) {
+        uint32_t rate = rates[i];
+        Serial.printf("[INFO]:Trying baud rate:%d\n", rate);
+        SerialAT.updateBaudRate(rate);
+        delay(10);
+        for (int j = 0; j < 10; j++) {
+            SerialAT.print("AT\r\n");
+            String input = SerialAT.readString();
+            if (input.indexOf("OK") >= 0) {
+                Serial.printf("[INFO]:Modem responded at rate:%d\n", rate);
+                SerialAT.println("AT+IPREX=115200");
+                return rate;
+            }
+        }
+    }
+    SerialAT.updateBaudRate(115200);
+    Serial.println("[ERROR]:Modem is not online!!!");
+    return 0;
+}
+
+
 void modem_on()
 {
     // Set-up modem  power pin
@@ -28,6 +55,11 @@ void modem_on()
     delay(10000);                 //Wait for the SIM7000 communication to be normal, and will quit when receiving OK
 
     int i = 10;
+
+    if(dectModemBaud() == 0){
+        Serial.println("Unable to communicate with modem.");while(1);
+    }
+
     Serial.println("\nTesting Modem Response...\n");
     Serial.println("****");
     while (i) {
